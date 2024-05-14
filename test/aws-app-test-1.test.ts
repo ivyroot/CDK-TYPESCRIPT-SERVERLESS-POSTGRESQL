1d31e3789f2804188921e88030673b4fc8c98488
ivyroot <ivyrootcode@gmail.com>
2024-05-14 16:04:13 -0700

    add shameful lack of tests

---
+++ test/aws-app-test-1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsAppTest1 from '../lib/aws-app-test-1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-app-test-1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsAppTest1.AwsAppTest1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
